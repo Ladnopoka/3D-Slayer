05df944d30a01a2fb0883925b6233902512244b9
Jevgenij Ivanov <ladnopokaa@gmail.com>
2023-10-26 20:11:41 +0100

    Can add rooms to scenes now
    Theres a bug with redo where room doesn't appear in node tree. Next stop is to make it so that I can add a room with all of its children to a scene, and not just the single node of a scene.

--- addons/editor-plugin/plugin.gd
+++ addons/editor-plugin/plugin.gd
@@ -2,20 +2,23 @@
 extends EditorPlugin
 
 const panel = preload("res://addons/editor-plugin/panel.tscn")
+const RoomTemplate = preload("res://addons/editor-plugin/room_template/room_template.tscn")
+
 var dockedScene
 
 # Get the undo/redo object
 var undo_redo = get_undo_redo()
 
 func _enter_tree():
-	add_custom_type("Button1", "Button1", preload("res://addons/editor-plugin/Button1.gd"), preload("res://icon.svg"))
 	dockedScene = panel.instantiate()
 	print("Panel scene instantiated!")
 	
 	var button1 = dockedScene.get_child(0)
 	var button2 = dockedScene.get_child(1)
+	var button3 = dockedScene.get_child(2)
 	button1.connect("pressed", create_wall)
 	button2.connect("pressed", create_box)
+	button3.connect("pressed", create_room)
 	
 	# Initial setup when the plugin is enabled
 	add_control_to_dock(DOCK_SLOT_RIGHT_UL, dockedScene)
@@ -100,3 +103,60 @@ func create_box():
 		box.owner = current_scene
 	else:
 		print("No active scene!")
+	
+func create_room():
+	var room = RoomTemplate.instantiate()
+	var current_scene = get_editor_interface().get_edited_scene_root()
+
+	if current_scene:
+		room.name = "Room_1" + str(current_scene.get_child_count())
+
+		# For undo/redo functionality:
+		undo_redo.create_action("Create Room")
+		undo_redo.add_do_method(current_scene, "add_child", room)
+		undo_redo.add_do_reference(room)
+		undo_redo.add_undo_method(current_scene, "remove_child", room)
+		undo_redo.commit_action(true)
+		room.owner = current_scene
+	else:
+		print("No active scene!")
+	
+	#Test function to add all childs to scene and not just the capsule	
+#func create_room():
+#	# Instantiate the room template
+#	var room_instance = RoomTemplate.instantiate()
+#
+#	# Get a reference to the current scene
+#	var current_scene = get_editor_interface().get_edited_scene_root()
+#
+#	if current_scene:
+#		# Create a new node to house the room's components
+#		var room_container = Node3D.new()
+#		room_container.name = "Room_" + str(current_scene.get_child_count() + 1)
+#
+#		var children_to_transfer = []
+#
+#		for child in room_instance.get_children():
+#			children_to_transfer.append(child)
+#
+#		for child in children_to_transfer:
+#			room_instance.remove_child(child)
+#			room_container.add_child(child)
+#
+#		# Begin undo-redo action
+#		undo_redo.create_action("Create Room")
+#
+#		# "Do" and "Undo" methods
+#		undo_redo.add_do_method(current_scene, "add_child", room_container)	
+#		undo_redo.add_undo_method(current_scene, "remove_child", room_container)
+#
+#		# Commit the action
+#		undo_redo.commit_action()
+#
+#		# Set ownership for the room_container and its children
+#		room_container.owner = current_scene
+#
+#		for child in room_container.get_children():
+#				child.owner = current_scene
+#	else:
+#		print("No active scene!")
