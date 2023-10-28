1d057dd87c6edbf4db3fe6063a0ed25206ccb9bb
Jevgenij Ivanov <ladnopokaa@gmail.com>
2023-10-28 18:05:45 +0100

    More deletions
    Errors are still present. Trying to isolate issue.

--- level/level_1.gd
+++ level/level_1.gd
@@ -10,20 +10,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_mob_timer_timeout():
-	# Create a new instance of the Mob scene.
-	var mob = mob_scene.instantiate()
-
-	# Choose a random location on the SpawnPath.
-	# We store the reference to the SpawnLocation node.
-	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
-	# And give it a random offset.
-	mob_spawn_location.progress_ratio = randf()
-
-	var player_position = $Rogue_Hooded.position
-	mob.initialize(mob_spawn_location.position, player_position)
-
-	# Spawn the mob by adding it to the Main scene.
-	add_child(mob)	
