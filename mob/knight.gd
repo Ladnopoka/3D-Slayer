46218128c400f91b23a5a10adc2ca65e1f227e7a
Jevgenij Ivanov <ladnopokaa@gmail.com>
2023-10-31 18:04:05 +0000

    Now looks at you

--- mob/knight.gd
+++ mob/knight.gd
@@ -11,8 +11,13 @@ func _ready():
 	
 func _process(delta):
 	velocity = Vector3.ZERO
+	
+	#navigation 
 	navigation_agent.set_target_position(player.global_transform.origin)
 	var next_nav_point = navigation_agent.get_next_path_position()
 	velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+	
+	look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
+	
 	move_and_slide()
 	
