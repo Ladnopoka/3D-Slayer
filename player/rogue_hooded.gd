0a60350ce68f8fb87452aaf327a952894d5d60ad
Jevgenij Ivanov <ladnopokaa@gmail.com>
2023-10-31 22:03:09 +0000

    Walking and shooting is kind of perfect now

--- player/rogue_hooded.gd
+++ player/rogue_hooded.gd
@@ -80,6 +80,8 @@ func _physics_process(delta):
 	if Input.is_action_pressed("primary_action"):
 		attack()
 	
+	anim_tree.set("parameters/conditions/run", walking)
+	
 func attack():
 	if walking:
 		return
