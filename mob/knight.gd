0dfa6b87111ac52a307ff91c24df4fd8d957150c
Jevgenij Ivanov <ladnopokaa@gmail.com>
2023-10-31 18:45:51 +0000

    Mobs now attack in black and behave better

--- mob/knight.gd
+++ mob/knight.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
 
 var player = null
+var state_machine
+
 const SPEED = 3.0
 const ATTACK_RANGE = 2.5
 
@@ -10,19 +12,25 @@ const ATTACK_RANGE = 2.5
 
 func _ready():
 	player = $"../../Rogue_Hooded"
+	state_machine = animation_tree.get("parameters/playback")
 	
 func _process(delta):
 	velocity = Vector3.ZERO
 	
-	#navigation 
-	navigation_agent.set_target_position(player.global_transform.origin)
-	var next_nav_point = navigation_agent.get_next_path_position()
-	velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
-	
-	look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
+	match state_machine.get_current_node():
+		"Running_A":
+			#navigation 
+			navigation_agent.set_target_position(player.global_transform.origin)
+			var next_nav_point = navigation_agent.get_next_path_position()
+			velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+			look_at(Vector3(global_position.x + velocity.x, global_position.y, 
+							global_position.z + velocity.z), Vector3.UP)
+		"1H_Melee_Attack_Chop":
+			look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
 	
 	#conditions
 	animation_tree.set("parameters/conditions/attack", _target_in_range())
+	animation_tree.set("parameters/conditions/run", !_target_in_range())
 	
 	move_and_slide()
 
