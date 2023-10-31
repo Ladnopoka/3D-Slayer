07f9789c50ca200e01f0caf28df021166c946de4
Jevgenij Ivanov <ladnopokaa@gmail.com>
2023-10-31 19:47:22 +0000

    Mobs now spawn in random locations

--- level/level2/level_2.gd
+++ level/level2/level_2.gd
@@ -1,6 +1,12 @@
 extends Node3D
 
 @onready var hit_rect = $Control/ColorRect
+@onready var spawns = $Spawns
+@onready var navigation_region = $NavigationRegion3D
+
+var knight = load("res://mob/knight.tscn")
+var knight_instance 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,3 +21,13 @@ func _on_rogue_hooded_player_hit():
 	hit_rect.visible = true
 	await get_tree().create_timer(0.2).timeout
 	hit_rect.visible = false
+
+func _get_random_child(parent_node):
+	var random_id = randi() % parent_node.get_child_count()
+	return parent_node.get_child(random_id) 
+
+func _on_knight_spawn_timer_timeout():
+	var spawn_point = _get_random_child(spawns).global_position
+	knight_instance = knight.instantiate()
+	knight_instance.position = spawn_point
+	navigation_region.add_child(knight_instance)
