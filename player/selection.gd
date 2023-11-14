2141e8d245ba72743bd252a4bc940d6b324b7e56
Jevgenij Ivanov <ladnopokaa@gmail.com>
2023-11-14 14:20:19 +0000

    Added cheer animation when character mage is selected

--- player/selection.gd
+++ player/selection.gd
@@ -1,6 +1,7 @@
 extends Area3D
 
 @onready var animation_player = $"../AnimationPlayer"
+@onready var animation_tree = $"../AnimationTree"
 @onready var character = get_parent()
 @onready var character_selection_scene = get_tree().get_root().get_node("Level1")
 
@@ -20,9 +21,11 @@ func _input_event(camera, event, position, normal, shape_idx):
 	
 	if Input.is_action_pressed("right_mouse_clicked"):
 		print("action pressed on character")
-		animation_player.play("Cheer")
+		animation_tree.set("parameters/AttackStateMachine/conditions/cheer", true)
 		emit_signal("character_selected", character.name)
 		$Selection.show()
+	elif Input.is_action_just_released("right_mouse_clicked"):
+		animation_tree.set("parameters/AttackStateMachine/conditions/cheer", false)
 		
 		
 func hide_selection():
