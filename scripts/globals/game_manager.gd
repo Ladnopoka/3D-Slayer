12986b2b4a598ce5d3c86871d9e5921e291e2ce8
Jevgenij Ivanov <ladnopokaa@gmail.com>
2023-11-14 14:58:15 +0000

    Selecting a character and going to level 2 now works

--- scripts/globals/game_manager.gd
+++ scripts/globals/game_manager.gd
@@ -1,6 +1,10 @@
 extends Node
 
 var player: Node
+var player_name: String
 
 func set_player(player_node):
 	player = player_node
+	
+func set_player_character(player_name):
+	self.player_name = player_name
