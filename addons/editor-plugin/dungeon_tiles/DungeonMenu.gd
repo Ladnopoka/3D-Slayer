de31d58376eb5aa58c8ecf2a2c22ffb5ec0d11b5
Jevgenij Ivanov <ladnopokaa@gmail.com>
2023-12-13 21:19:53 +0000

    Working on dungeonmenu.gd code

--- addons/editor-plugin/dungeon_tiles/DungeonMenu.gd
+++ addons/editor-plugin/dungeon_tiles/DungeonMenu.gd
@@ -2,9 +2,13 @@
 extends Node3D
 
 @export var start : bool = false : set = set_start
-
 func set_start(val:bool)->void:
 	generate()
+	
+
+@export var border_size : int = 20 : set = set_border_size
+func set_border_size(val : int) -> void:
+	border_size = val
 	
 func generate():
 	print("generating")
