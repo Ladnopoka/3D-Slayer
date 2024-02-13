20ae32f1c7c6924f63e1ad31da5c3f62e0d183ad
Jevgenij Ivanov <ladnopokaa@gmail.com>
2024-02-13 23:21:59 +0000

    zombies now disappear after getting hit 3 times by mage

---
+++ mob/zombie/Area3D.gd
@@ -0,0 +1,17 @@
+extends Area3D
+
+@export var damage := 1
+
+signal zombie_hit(damage)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func hit():
+	emit_signal("zombie_hit", damage)
--- player/mage/mage_skill.gd
+++ player/mage/mage_skill.gd
@@ -2,6 +2,10 @@ extends Node3D
 
 @export var speed = 50
 
+@onready var mesh = $MeshInstance3D
+@onready var ray = $RayCast3D
+@onready var particles = $GPUParticles3D
+
 const KILL_TIME = 2
 var timer = 0
 
@@ -10,6 +14,17 @@ func _physics_process(delta):
 	global_translate(forward_direction * speed * delta)
 	
 	timer += delta
-	
 	if timer >= KILL_TIME:
 		queue_free()
+		
+	if ray.is_colliding():
+		mesh.visible = false
+		particles.emitting = true
+		ray.enabled = false
+		if ray.get_collider().is_in_group("enemy"):
+			ray.get_collider().hit()
+		await get_tree().create_timer(1.0).timeout
+		queue_free()
+
+func _on_timer_timeout():
+	queue_free()
