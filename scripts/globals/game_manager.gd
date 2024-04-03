10080939156dff786b7bcf0d9c3b6293d569a0d2
Jevgenij Ivanov <ladnopokaa@gmail.com>
2024-04-03 23:13:52 +0100

    camera now jumps to other characters but very badly

--- scripts/globals/game_manager.gd
+++ scripts/globals/game_manager.gd
@@ -3,9 +3,16 @@ extends Node
 var player: Node
 var player_name: String
 
+signal active_character_changed(new_player)
+
 func set_player(player_node):
 	player = player_node
 	print("Global Player: ", player, " Player Name: ", self.player_name)
 	
 func set_player_character(player_name):
 	self.player_name = player_name
+
+func set_active_character(character):
+	player = character
+	# You may also want to emit a signal indicating the player has changed, in case other parts of your game rely on this information
+	emit_signal("active_character_changed", player)
--- scripts/player/camera_rig.gd
+++ scripts/player/camera_rig.gd
@@ -9,12 +9,22 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	resize()
+	GameManager.connect("active_character_changed", _on_active_character_changed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	background_camera.global_transform = GameManager.player.camera_point.global_transform
-	foreground_camera.global_transform = GameManager.player.camera_point.global_transform
+	if GameManager.player:
+		background_camera.global_transform = GameManager.player.camera_point.global_transform
+		foreground_camera.global_transform = GameManager.player.camera_point.global_transform
+	else:
+		print("Camera can't find player!")
 	
 func resize():
 	background_viewport.size = DisplayServer.window_get_size()
 	foreground_viewport.size = DisplayServer.window_get_size()
+
+func _on_active_character_changed(new_player):
+	print("Active character changed to: ", new_player.name)
+	# Update the camera to follow the new player
+	background_camera.global_transform = new_player.camera_point.global_transform
+	foreground_camera.global_transform = new_player.camera_point.global_transform
