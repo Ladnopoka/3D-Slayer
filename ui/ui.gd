d4481aa033bf7ae536988a3c621c8c6b0bda9670
Jevgenij Ivanov <ladnopokaa@gmail.com>
2024-06-03 13:57:23 +0100

    checkpoint before adding level up effect

--- ui/ui.gd
+++ ui/ui.gd
@@ -5,22 +5,23 @@ extends CanvasLayer
 @onready var label = $HealthGlobe/Label
 
 var player
+var level_num = 0
 
 #Exp
 @onready var experience_bar = %"Experience Bar"
 @onready var experience_label = %"Experience Label"
 
-func set_expbar(set_value = 1, set_max_value = 100):
-	experience_bar.value = set_value
-	experience_bar.max_value = set_max_value
+#func set_expbar(set_value = 1, set_max_value = 100):
+	#experience_bar.value = set_value
+	#experience_bar.max_value = set_max_value
+#
+#func _on_knight_died(xp_reward: int):
+	#player.gain_experience(xp_reward)
+	#_update_experience_bar()
 
-func _on_knight_died(xp_reward: int):
-	player.gain_experience(xp_reward)
-	_update_experience_bar()
-
-func _update_experience_bar():
-	experience_bar.max_value = 100
-	experience_bar.value = player.current_exp
+#func _update_experience_bar():
+	#experience_bar.max_value = 100
+	#experience_bar.value = player.current_exp
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -28,9 +29,7 @@ func _ready():
 	health_globe.max_value = player.hp
 	health_globe.value = player.hp
 	label.text = str(player.current_hp) + "/" + str(player.hp)
-	
-	set_expbar(1, 100)
-
+	experience_label.text = "Level: " + str(level_num)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -45,3 +44,7 @@ func UpdateGlobes():
 func UpdateExp():
 	var new_exp = player.current_exp
 	experience_bar.value = new_exp
+	
+	if experience_bar.value >= 100:
+		experience_bar.value = 0
+		experience_label.text = "Level: " + str(level_num)
