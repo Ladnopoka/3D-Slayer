70a3fe351a0308be8b0af7703aebfb04e7f7f59b
Jevgenij Ivanov <ladnopokaa@gmail.com>
2024-06-07 21:36:40 +0100

    inventory now opens up

--- inventory/inventory_dialog.gd
+++ inventory/inventory_dialog.gd
@@ -7,6 +7,9 @@ extends PanelContainer
 func open(inventory:Inventory):
 	show()
 	
+	for child in grid_container.get_children():
+		child.queue_free()
+	
 	for item in inventory.get_items():
 		var slot = slot_scene.instantiate()
 		grid_container.add_child(slot)
