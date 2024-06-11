456cb839078067e9f241940828b7f0bc76c91105
Jevgenij Ivanov <ladnopokaa@gmail.com>
2024-06-11 15:41:47 +0100

    items now appear instantly when picked up

--- inventory/inventory.gd
+++ inventory/inventory.gd
@@ -2,11 +2,16 @@ class_name Inventory
 
 var _content:Array[Item] = []
 
+signal item_added(item: Item)
+signal item_removed(item: Item)
+
 func add_item(item:Item):
 	_content.append(item)
+	emit_signal("item_added", item)
 	
 func remove_item(item:Item):
 	_content.erase(item)
+	emit_signal("item_removed", item)
 	
 func get_items() -> Array[Item]:
 	return _content
--- inventory/inventory_dialog.gd
+++ inventory/inventory_dialog.gd
@@ -4,10 +4,21 @@ extends PanelContainer
 @export var slot_scene:PackedScene
 @onready var grid_container:ItemGrid = %GridContainer
 
+var _inventory:Inventory
+
 func open(inventory:Inventory):
+	_inventory = inventory
 	show()
+	
+	# Connect signals
+	_inventory.connect("item_added",  _on_item_changed)
+	_inventory.connect("item_removed", _on_item_changed)
 	
 	grid_container.display(inventory.get_items())
 
 func _on_close_button_pressed():
 	hide()
+
+func _on_item_changed(item:Item):
+	# Refresh the inventory display
+	grid_container.display(_inventory.get_items())
