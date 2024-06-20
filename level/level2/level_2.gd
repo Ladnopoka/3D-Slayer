f6915702a8a657874102129a1b9505c5728ca7eb
Jevgenij Ivanov <ladnopokaa@gmail.com>
2024-06-20 11:16:05 +0100

    stuff work but broken

--- level/level2/level_2.gd
+++ level/level2/level_2.gd
@@ -5,6 +5,7 @@ extends Node3D
 @onready var navigation_region = $NavigationRegion3D
 @onready var transition = $Transition
 @onready var score = $Score
+@onready var wave_timer = $WaveTimer
 
 var knight = load("res://mob/knight.tscn")
 var knight_instance
@@ -16,6 +17,11 @@ const ZOMBIE = preload("res://mob/zombie/zombie.tscn")
 var zombie_instance
 
 var player
+
+var wave_interval = 2  # seconds between each wave
+var current_time = 0
+var wave_number = 0
+var max_waves = 10  # Adjust based on your game's design
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,32 +35,54 @@ func _ready():
 	player_ins.current_hp = GameState.player_data["health"]
 	player_ins.level = GameState.player_data["level"]
 	transition.get_node("AnimationPlayer").play("fade_in")
+	
+	# Setup Wave Timer
+	wave_timer.wait_time = wave_interval
+	if !wave_timer.is_connected("timeout", _on_wave_timer_timeout):
+		wave_timer.connect("timeout", _on_wave_timer_timeout)
+	wave_timer.start()
+
+func _on_wave_timer_timeout():
+	if wave_number < max_waves:
+		wave_number += 1
+		spawn_wave()
+		wave_timer.start()  # Restart the timer for the next wave
+		
+func spawn_wave():
+	var spawn_count = wave_number * 2  # Adjust based on desired difficulty scaling
+	for i in range(spawn_count):
+		var spawn_point = _get_random_child(spawns).global_position
+		var enemy_instance = choose_random_enemy().instantiate()
+		enemy_instance.position = spawn_point
+		navigation_region.add_child(enemy_instance)
+		
+func choose_random_enemy():
+	var enemies = [ZOMBIE, knight] # Add all your enemy types here IMP, ZOMBIE, knight
+	return enemies[randi() % enemies.size()]		
+		
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	score.text = "Knights Killed: " + str(Global.score) + "/100"
-
-
-#func _on_rogue_hooded_player_hit():
-	#hit_rect.visible = true
-	#await get_tree().create_timer(0.2).timeout
-	#hit_rect.visible = false
+	current_time += _delta  # Increment the current time
 
 func _get_random_child(parent_node):
 	var random_id = randi() % parent_node.get_child_count()
 	return parent_node.get_child(random_id) 
 
 func _on_knight_spawn_timer_timeout():
-	var spawn_point = _get_random_child(spawns).global_position
+	#var spawn_point = _get_random_child(spawns).global_position
 	#imp_instance = IMP.instantiate()
 	#imp_instance.position = spawn_point
 	#navigation_region.add_child(imp_instance)
 	#knight_instance = knight.instantiate()
 	#knight_instance.position = spawn_point
 	#navigation_region.add_child(knight_instance)
-	zombie_instance = ZOMBIE.instantiate()
-	zombie_instance.position = spawn_point
-	navigation_region.add_child(zombie_instance)
+	#zombie_instance = ZOMBIE.instantiate()
+	#zombie_instance.position = spawn_point
+	#navigation_region.add_child(zombie_instance)
+	pass
 	
 func get_player_character():
 	print("GameManager Player Name: ", GameManager.player_name)
@@ -67,6 +95,3 @@ func get_player_character():
 			player = load("res://player/mage/mage.tscn")
 		_:
 			player = load("res://player/barbarian/barbarian.tscn")
-	#player = load("res://player/rogue/rogue_hooded.tscn")
-	#print(GameManager.player)
-	#print("Player Name: ", GameManager.player_name, " Player: ", GameManager.player)
